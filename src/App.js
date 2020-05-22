cbe4111addb761eaea703bbcb332dd3973f831fd
crashdaddy <kennethscoggins@mail.com>
2020-05-22 09:40:08 -0500

    new banner, data panel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import bierGirl from './img/bierGirl.png'; // with import
 import './App.css';
+
+function Header() {
+  return (
+    <div className="bannerDiv">
+      <img src={bierGirl} className="bannerPic"/>
+      <div className="title">Bier Bitte!</div>
+      <div className="tagline">"One drink's not going to kill you..."</div>
+    </div>
+
+  )
+}
 
 class Likes extends Component {
  
@@ -9,6 +20,28 @@ class Likes extends Component {
       <div className="likesCounter">Liked: {this.props.value}</div>
     )
     }
+}
+
+class Nutrition extends Component {
+  render() {
+    return(
+    <div className="nutritionLabel">
+        <div style={{fontFamily: "franklinGothic", fontHeight: '40px'}}><h1>Nutrition Facts</h1></div>
+    </div>
+    )
+  }
+}
+
+class Facts extends Component {
+   render() {
+     return (
+      <div className="factsPanel">
+        <h1>{this.props.currentBeer.name}</h1>
+        <Nutrition beer={this.props.currentBeer} />
+      </div>
+
+     )
+   }
 }
 
 class Beer extends Component {
@@ -26,10 +59,14 @@ class Beer extends Component {
     })
     this.props.updateLikes(this.state.liked)
   }
+
+  changeBeer = () => {
+    this.props.changeBeer(this.props.beer);
+  }
   
   render() {
   return (
-    <div className="beerDiv">
+    <div className="beerDiv" onClick={this.changeBeer}>
     <div><h1>{this.props.beer.name}</h1></div>
     <img src={this.props.beer.image_url} alt={this.props.beer.name} style={{width: '30px', float: 'left',paddingRight: '10px'}}/>
     {this.props.beer.description}
@@ -47,13 +84,17 @@ class App extends Component {
     // class-based Components allow us to have "state"! And this is why/when we use class-based components.
     this.state = {
         beers: [],
+        currentBeer: {},
         page: 1,
         likes : 0
     }
   }
 
+  changeBeer = (newBeer) => {
+      this.setState({currentBeer: newBeer})
+  }
+
   updatelikes = (liked) => {
-    console.log(liked)
     let currentLikes = this.state.likes;
     if (liked===true) { 
       currentLikes--;
@@ -106,8 +147,12 @@ render() {
 
     return (
       <div>
-     {this.state.beers.map((beerdata,idx) => (<Beer key={idx} beer={beerdata} updateLikes={this.updatelikes} />))}
+      <Header/>
+      <div className="outputDiv">
+     {this.state.beers.map((beerdata,idx) => (<Beer key={idx} beer={beerdata} changeBeer={this.changeBeer} updateLikes={this.updatelikes} />))}
+     </div>
      <Likes value={this.state.likes} />
+     <Facts currentBeer={this.state.currentBeer}/>
      </div>
     );
   }
